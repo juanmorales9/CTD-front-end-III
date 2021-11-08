008657d54a1502de16704d5625986d4566c4846d
Eduardo de Araújo <eduardoaraujogf@gmail.com>
2021-11-08 20:11:42 -0300

    Aula 08 - Componentes Declarados

--- aula-08/src/App.jsx
+++ aula-08/src/App.jsx
@@ -3,7 +3,6 @@ import { Component } from 'react';
 import Increment from './components/Increment';
 import Toggler from './components/Toggler';
 import RemoveItemFromList from './components/RemoveItemFromList';
-
 import './App.scss';
 
 export default class App extends Component {
